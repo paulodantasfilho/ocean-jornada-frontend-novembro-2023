16e04685a35330d7a4843848831a8da1fbecc0ad
Paulo Dantas Filho <111999609+paulodantasfilho@users.noreply.github.com>
2023-11-14 01:11:37 +0000

    Criação dos objetos

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import './App.css'
 import Card from './components/Card/Card';
 
 function App() {
+  const item1 = {
+    nome:"Rick Sanchez",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+  }
+
+  const item2 = {
+    name: "Morte Saith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+  }
     return (
-    <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-        <div className="tags">
-          <div className="tag">Status: Vivo</div>
-          <div className="tag">Espécie: Humana</div>
-          <div className="tag">Origem: Terra</div>
-        </div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
-      </div>
+    <>      
       <br />
-      <Card />
+      <Card item={item1} />
+      <Card item={item2} />
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,3 +1,13 @@
-export default function Card() {
-    return <div>Card está funcionando!</div>
+export default function Card(props) {
+    const item = props.item
+
+    return <div className="card">
+    <h2>{item.name}</h2>
+    <div className="tags">
+      <div className="tag">Status: Vivo</div>
+      <div className="tag">Espécie: Humana</div>
+      <div className="tag">Origem: Terra</div>
+    </div>
+    <img src={item.imageUrl} />
+  </div>
   }
